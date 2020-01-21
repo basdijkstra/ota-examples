bc6b5ffbb64d26572be0478e972fbfeb01f6c012
Bas Dijkstra <bas@ontestautomation.com>
2020-01-21 12:51:36 +0100

    Added example unmarshalling JSON response and asserting on a field

--- golang-resty/zippopotamus_api_test.go
+++ golang-resty/zippopotamus_api_test.go
@@ -1,10 +1,16 @@
 package golang_resty_examples
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/go-resty/resty"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
+
+type LocationResponse struct {
+	Country string `json:"country"`
+}
 
 func Test_GetUs90210_StatusCodeShouldEqual200(t *testing.T) {
 
@@ -33,5 +39,23 @@ func Test_GetUs90210_ContentTypeShouldEqualApplicationJson(t *testing.T) {
 	resp, _ := client.R().Get("http://api.zippopotam.us/us/90210")
 
 	assert.Equal(t, "application/json", resp.Header().Get("Content-Type"))
+}
+
+func Test_GetUs90210_CountryShouldEqualUnitedStates(t *testing.T) {
+
+	client := resty.New()
+
+	resp, _ := client.R().Get("http://api.zippopotam.us/us/90210")
+
+	myResponse := LocationResponse{}
+
+	err := json.Unmarshal(resp.Body(), myResponse)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	assert.Equal(t, "United States", myResponse.Country)
 }
 
